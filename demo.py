852bea1d9cf5af6aead176854450ef93b32bff59
DW <weikert-deluxe@gmx.de>
2022-05-11 10:55:34 +0200

    Second does its own thing

--- demo.py
+++ demo.py
@@ -5,8 +5,14 @@ app =  Flask()
 df =pd.read_csv("customers.csv")
 print(df.head())
 print('done')
+<<<<<<< HEAD
 assert 8==8
 
 #added some comments
 
 #added some more comments
+=======
+
+
+#second branch does different things
+>>>>>>> Second does its own thing
